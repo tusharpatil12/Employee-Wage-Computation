0d66b03814ad95513eb70fb13452d670097d2197
tusharpatil12 <tusharpatil22700@gmail.com>
2022-08-15 11:41:42 -0700

    UC1 commit

--- src/CalculateEmpWage.java
+++ src/CalculateEmpWage.java
@@ -2,7 +2,14 @@
 public class CalculateEmpWage {
 
 	public static void main(String[] args) {
-	  System.out.println("Welcome to Employee wage Computation Problem");	    		
+	  System.out.println("Welcome to Employee wage Computation Problem");
+	  double empCheck = Math.floor(Math.random()*10%2);
+	  System.out.println(empCheck);
+	  	if(empCheck == 1)
+	  		System.out.println("The employee is present");
+	  	else
+	  		System.out.println("The employee is absent");
+	  
 	}
 
 }

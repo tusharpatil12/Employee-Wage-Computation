71e3cd73a5697ff759a9814cfcc3c2208acd66d8
tusharpatil12 <tusharpatil22700@gmail.com>
2022-08-15 11:28:01 -0700

    https://github.com/tusharpatil12/Employee-Wage-Computation.git

--- src/CalculateEmpWage.java
+++ src/CalculateEmpWage.java
@@ -2,17 +2,7 @@
 public class CalculateEmpWage {
 
 	public static void main(String[] args) {
-	    double empCheck = ((Math.random())*10%2);
-	    System.out.println(empCheck);
-	    double flag = Math.floor(empCheck);	    
-	    		System.out.println(flag);
-	    		if(flag == 1) {
-	    			System.out.println("Employee is present");
-	    		}else {
-	    			System.out.println("Employee is absent");
-	    		}
-	    			
-	    		
+	  System.out.println("Welcome to Employee wage Computation Problem");	    		
 	}
 
 }

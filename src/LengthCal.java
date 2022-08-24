ebe9262ebb91098d0ce20286c748638490633b4d
tusharpatil12 <tusharpatil22700@gmail.com>
2022-08-23 17:44:06 -0700

    The my first commit

--- src/LengthCal.java
+++ src/LengthCal.java
@@ -3,10 +3,10 @@ public class LengthCal {
 
 	public static void main(String[] args) {
 		int x1,x2,y1,y2;
-		x1=2;x2=5;y1=4;y2=8;
+		x1=2;x2=5;
+		y1=4;y2=8;
 		
 		double distance=Math.sqrt(x2-x1)*2 +(y2-y1)*2;
 		System.out.println("distance between = "+"("+x1+","+y1+"),"+"("+x2+","+y2+") "+distance);
 	}
-
 }
